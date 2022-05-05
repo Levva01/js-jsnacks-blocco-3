06792b07f4e76cacc1e7982a55e07b0bc54fb2bc
Levva01 <vellanifederico13@gmail.com>
2022-05-05 14:50:06 +0200

    SNACK 1

--- snack-1/main.js
+++ snack-1/main.js
@@ -4,7 +4,8 @@ let array1 = [];
 array1.push(Number(prompt("Inserisci un numero")));
 
 //creo una variabile che terrà conto della somma degli elementi dell'array
-let sum = array1[0];
+let sum = 0;
+sum = sum + array1[0];
 
 let i = 1;
 
@@ -12,13 +13,16 @@ while(sum < 50){
     
         array1.push(Number(prompt("Inserisci un numero")));
         sum = sum + array1[i];
-        i++;
+        console.log(sum);
 
         if(sum > 49){
-            array1.pop([i]);
             sum = sum - array1[i];
+            console.log(sum);
+            array1.pop([i]);
             break;
         }
+
+        i++;
     
 }
 
